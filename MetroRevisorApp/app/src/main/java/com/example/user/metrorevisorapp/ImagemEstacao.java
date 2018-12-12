f568c1b9cec70d2beec42c69184d656af19b9237
Carolina <carolveloso97@gmail.com>
2018-12-12 19:11:30 +0000

    Eu sou bue linda

---
+++ MetroRevisorApp/app/src/main/java/com/example/user/metrorevisorapp/ImagemEstacao.java
@@ -0,0 +1,13 @@
+package com.example.user.metrorevisorapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImagemEstacao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imagem_estacao);
+    }
+}
---
+++ MetroRevisorApp/app/src/main/java/com/example/user/metrorevisorapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.user.metrorevisorapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn_login = (Button) findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent (getApplicationContext(), Menu_Principal.class);
+                startActivity(startIntent);
+            }
+        });
+    }
+}
